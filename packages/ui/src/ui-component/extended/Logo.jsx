3b5e2f09a7a85ad84a558ae47e0384aab0156d1f
The Reverend <reverend@thewatchersociety.org>
2024-05-01 18:30:21 -0500

    update logo

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+import logo from '@/assets/images/logo_site_light.png'
+import logoDark from '@/assets/images/logo_site_dark.png'
 
 import { useSelector } from 'react-redux'
 
